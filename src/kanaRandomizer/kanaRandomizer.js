c2de749a0d105082bef95b52d53e7a145118fac7
carlosalmira-aircury <carlos.almira@aircury.com>
2024-05-22 15:53:39 +0200

    Important changes

--- src/kanaRandomizer/kanaRandomizer.js
+++ src/kanaRandomizer/kanaRandomizer.js
@@ -1,12 +1,24 @@
 
-function randomizer(){
-    
+function randomizer(array,translationsArray){
+  const newMainArray = []
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];// Changing Elements
+          [translationsArray[i], translationsArray[j]] = [translationsArray[j], translationsArray[i]];
+        }
+        newMainArray.push(array);
+        newMainArray.push(translationsArray);
+        return newMainArray;
 }
-export function kanaRandomizer(kanaArray){
+export function kanaRandomizer(kanaArray,kanaTranslations){
 let newArray = [];
+let newTranslationsArray = [];
     kanaArray.map((element)=>{
         newArray.push(...element)
     })
-
-    console.log(newArray);
+    kanaTranslations.map((element)=>{
+      newTranslationsArray.push(...element)
+  })
+    console.log('newArray',newArray);
+    return randomizer(newArray,newTranslationsArray);
 }
